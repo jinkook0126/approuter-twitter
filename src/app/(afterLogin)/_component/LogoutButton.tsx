b5bb89b92c6a334429b4dfaac019e4d0c03eedb3
이진국 <jinkook0126@naver.com>
2024-05-20 22:23:14 +0900

    feat: 로그아웃 시 캐시정리

--- src/app/(afterLogin)/_component/LogoutButton.tsx
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -25,6 +25,7 @@ export default function LogoutButton({ me }: Props) {
         method: "post",
         credentials: "include",
       });
+      router.refresh();
       router.replace("/");
     });
   };
