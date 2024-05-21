229d58ed8cafe7137110fa58067f840f5aa745f5
이진국 <jinkook0126@naver.com>
2024-05-22 00:12:04 +0900

    feat: 리버스 인피니티 스크롤과 소켓 io를 이용한 채팅구현

--- src/app/(afterLogin)/messages/[room]/page.tsx
+++ src/app/(afterLogin)/messages/[room]/page.tsx
@@ -1,82 +1,39 @@
-import { faker } from "@faker-js/faker";
 import style from "./chatRoom.module.css";
-import Link from "next/link";
-import BackButton from "@/app/(afterLogin)/_component/BackButton";
-import cx from "classnames";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
 import dayjs from "dayjs";
+import { MessageForm } from "./_component/MessageForm";
+import { auth } from "@/auth";
+import { getUserServer } from "../../[username]/_lib/getUserServer";
+import { UserInfo } from "./_component/UserInfo";
+import { QueryClient } from "@tanstack/react-query";
+import WebSocketComponent from "./_component/WebSocketComponent";
+import MessageList from "./_component/MessageList";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
 
-export default function ChatRoom() {
-  const user = {
-    id: "hero",
-    nickname: "영웅",
-    image: faker.image.avatar(),
-  };
-  const messages = [
-    {
-      messageId: 1,
-      roomId: 123,
-      id: "zerohch0",
-      content: "안녕하세요.",
-      createdAt: new Date(),
-    },
-    {
-      messageId: 2,
-      roomId: 123,
-      id: "hero",
-      content: "안녕히가세요.",
-      createdAt: new Date(),
-    },
-  ];
+type Props = {
+  params: { room: string };
+};
 
+export default async function ChatRoom({ params }: Props) {
+  const session = await auth();
+  const queryClient = new QueryClient();
+  const ids = params.room.split("-").filter((v) => v !== session?.user?.email);
+  if (!ids[0]) {
+    return null;
+  }
+  await queryClient.prefetchQuery({
+    queryKey: ["users", ids[0]],
+    queryFn: getUserServer,
+  });
   return (
     <main className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <div>
-          <h2>{user.nickname}</h2>
-        </div>
-      </div>
-      <Link href={user.nickname} className={style.userInfo}>
-        <img src={user.image} alt={user.id} />
-        <div>
-          <b>{user.nickname}</b>
-        </div>
-        <div>@{user.id}</div>
-      </Link>
-      <div className={style.list}>
-        {messages.map((m) => {
-          if (m.id === "zerohch0") {
-            // 내 메시지면
-            return (
-              <div
-                key={m.messageId}
-                className={cx(style.message, style.myMessage)}
-              >
-                <div className={style.content}>{m.content}</div>
-                <div className={style.date}>
-                  {dayjs(m.createdAt).format("YYYY년 MM월 DD일 A HH시 mm분")}
-                </div>
-              </div>
-            );
-          }
-          return (
-            <div
-              key={m.messageId}
-              className={cx(style.message, style.yourMessage)}
-            >
-              <div className={style.content}>{m.content}</div>
-              <div className={style.date}>
-                {dayjs(m.createdAt).format("YYYY년 MM월 DD일 A HH시 mm분")}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <WebSocketComponent />
+      <UserInfo id={ids[0]} />
+      <MessageList id={ids[0]} />
+      <MessageForm id={ids[0]} />
     </main>
   );
 }
--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,24 +1,27 @@
 import { Metadata } from "next";
 import style from "./message.module.css";
 import Room from "@/app/(afterLogin)/messages/_component/Room";
+import { auth } from "@/auth";
+import { getRooms } from "./_lib/getRooms";
 
 export const metadata: Metadata = {
   title: "쪽지 / Z",
   description: "쪽지를 보내세요.",
 };
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  const rooms = session?.user?.email
+    ? await getRooms(session?.user?.email)
+    : [];
   return (
     <main className={style.main}>
       <div className={style.header}>
         <h3>쪽지</h3>
       </div>
-      <Room />
-      <Room />
-      <Room />
-      <Room />
-      <Room />
-      <Room />
+      {rooms.map((room) => (
+        <Room key={room.room} room={room} />
+      ))}
     </main>
   );
 }
